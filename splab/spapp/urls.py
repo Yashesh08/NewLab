b4a2479544d93d8f7d4abf41c00a9be62adb9f9d
Yashesh_213 <141909338+Yashesh08@users.noreply.github.com>
2026-02-19 14:31:47 +0530

    Add login and register pages for user side

--- splab/spapp/urls.py
+++ splab/spapp/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('courses/<slug:slug>/', views.course_detail, name='course_detail'),
     path('instructors/', views.instructors, name='instructors'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
 ]
